c728b3736514aa09313a06575ca2dfdfc422f830
Zouine2005 <zouinemohamade@gmail.com>
2024-04-20 11:48:01 +0100

    for get the filiere id in format json

---
+++ PFE-app/app/Http/Controllers/FiliereApiController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Filiere;
+use App\Models\Groupe;
+use Illuminate\Http\Request;
+
+class FiliereApiController extends Controller
+{
+    public function getFilieres($formateurId)
+    {
+        $filieres = Filiere::where('id_formateur', $formateurId)->get();
+
+        return response()->json($filieres);
+    }
+
+    public function getGroupesByFiliere($filiereId)
+    {
+        // Récupérer les groupes associés à la filière
+        $groupes = Groupe::where('filiere_id', $filiereId)->get();
+
+        return response()->json($groupes);
+    }
+
+   
+}
